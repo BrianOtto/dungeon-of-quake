6020a7ac1d213f319338977ad4afe3ad2e437ade
BrianOtto <brian.otto@zoho.com>
2022-10-21 20:00:41 -0700

    Updated the build script to treat main.odin as a standalone package

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 @echo off
 
 echo build doq...
-call odin build main.odin -out:build/dungeon_of_quake.exe -o:speed -strict-style -vet && if "%1%"=="run" (echo run... && cd build && call dungeon_of_quake.exe) else (echo done)
+call odin build main.odin -file -out:build/dungeon_of_quake.exe -o:speed -strict-style -vet && if "%1%"=="run" (echo run... && cd build && call dungeon_of_quake.exe) else (echo done)
